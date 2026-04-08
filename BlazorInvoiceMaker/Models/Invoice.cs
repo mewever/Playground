18f1b2e576f0682956504a677c5ee4d8ed9dc75e
Mike Wever <mewever@gmail.com>
2026-04-08 16:26:58 -0500

    Added BlazorInvoiceMaker project

---
+++ BlazorInvoiceMaker/Models/Invoice.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorInvoiceMaker.Models
+{
+    public class Invoice
+    {
+        public string InvoiceNumber { get; set; } = string.Empty;
+        public string ReferenceNumber { get; set; } = string.Empty;
+        public DateTime InvoiceDate { get; set; } = DateTime.Now;
+        public DateTime DueDate { get; set; } = DateTime.Now.AddDays(30);
+        public string PayeeName { get; set; } = string.Empty;
+        public string BillingAddress { get; set; } = string.Empty;
+        public string ShippingAddress { get; set; } = string.Empty;
+        public decimal Subtotal { get; set; }
+        public decimal ShippingFees { get; set; }
+        public decimal OtherFees { get; set; }
+        public decimal Taxes { get; set; }
+        public decimal Total {
+            get
+            {
+                return Subtotal + ShippingFees + OtherFees + Taxes;
+            }
+        }
+        public List<InvoiceLine> Lines { get; set; } = [];
+    }
+}
---
+++ BlazorInvoiceMaker/Models/InvoiceLine.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorInvoiceMaker.Models
+{
+    public class InvoiceLine
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public decimal ExtendedPrice
+        {
+            get
+            {
+                return Quantity * UnitPrice;
+            }
+        }
+    }
+}
